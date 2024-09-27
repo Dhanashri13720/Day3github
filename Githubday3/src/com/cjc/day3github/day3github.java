dade6e66033b8c4b519d47ae5564fd61a32fd74c
ADMIN <ADMIN@DESKTOP-0GO5QNF>
2024-09-27 10:21:23 +0530

    By Dhanashri

---
+++ Githubday3/src/com/cjc/day3github/day3github.java
@@ -0,0 +1,13 @@
+package com.cjc.day3github;
+
+public class day3github {
+public static void main(String[]args)
+{
+	System.out.println("Welcome to github");
+}
+
+public void m1()
+{
+	System.out.println("Integrate gits to jenkins");
+}
+}
